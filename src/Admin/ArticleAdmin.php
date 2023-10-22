3a8f80c45d09598f424918faefe84db1617fe1cf
Rumil <cannirumil@yandex.ru>
2023-10-23 01:17:29 +0300

    fixes

--- src/Admin/ArticleAdmin.php
+++ src/Admin/ArticleAdmin.php
@@ -2,22 +2,37 @@
 
 namespace App\Admin;
 
+use App\Entity\Category;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 final class ArticleAdmin extends AbstractAdmin
 {
+    private const NAME_LABEL = 'Название';
+    private const SLUG_LABEL = 'Уникальный фрагмент URL-адреса';
+    private const CONTENT_LABEL = 'Cодержание';
+    private const CATEGORIES_LABEL = 'Категории';
+
     protected function configureFormFields(FormMapper $form): void
     {
         $form
-            ->add('title', TextType::class, ['label' => 'Название'])
-            ->add('slug', TextType::class, ['label' => 'Уникальный фрагмент URL-адреса'])
-            ->add('content', TextareaType::class, ['label' => 'Cодержание']);
+            ->add('title', TextType::class, ['label' => self::NAME_LABEL])
+            ->add('slug', TextType::class, ['label' => self::SLUG_LABEL])
+            ->add('content', TextareaType::class, ['label' => self::CONTENT_LABEL])
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
+                'choice_label' => 'name',
+                'label' => self::CATEGORIES_LABEL
+            ]);
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagrid): void
--- src/Admin/CategoryAdmin.php
+++ src/Admin/CategoryAdmin.php
@@ -13,57 +13,47 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 final class CategoryAdmin extends AbstractAdmin
 {
+    private const NAME_LABEL = 'Название';
+    private const SLUG_LABEL = 'Уникальный фрагмент URL-адреса';
+    private const COLOR_LABEL = 'Цвет';
 
     protected function configureFormFields(FormMapper $form): void
     {
         $form
-            ->add('name', TextType::class, ['label' => $this->getParamLabel('name')])
-            ->add('slug', TextType::class, ['label' => $this->getParamLabel('slug')])
+            ->add('name', TextType::class, ['label' => self::NAME_LABEL])
+            ->add('slug', TextType::class, ['label' => self::SLUG_LABEL])
             ->add('color', ChoiceFieldMaskType::class, [
                 'multiple' => false,
                 'choices' => array_flip($this->getColors()),
-                'label' => $this->getParamLabel('color')
+                'label' => self::COLOR_LABEL
             ]);
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagrid): void
     {
         $datagrid
-            ->add('name', TextType::class, ['label' => $this->getParamLabel('name')])
-            ->add('slug', TextType::class, ['label' => $this->getParamLabel('slug')])
-            ->add('color', TextType::class, ['label' => $this->getParamLabel('color')]);
+            ->add('name', null, [], ['label' => self::NAME_LABEL])
+            ->add('slug', null, [], ['label' => self::SLUG_LABEL])
+            ->add('color', null, [], ['label' => self::COLOR_LABEL]);
     }
 
     protected function configureListFields(ListMapper $list): void
     {
         $list
-            ->addIdentifier('name', TextType::class, ['label' => $this->getParamLabel('name')])
-            ->add('slug', TextType::class, ['label' => $this->getParamLabel('slug')])
-            ->add('color', TextType::class, ['label' => $this->getParamLabel('color')]);
+            ->addIdentifier('name', TextType::class, ['label' => self::NAME_LABEL])
+            ->add('slug', TextType::class, ['label' => self::SLUG_LABEL])
+            ->add('color', TextType::class, ['label' => self::COLOR_LABEL]);
     }
 
     protected function configureShowFields(ShowMapper $show): void
     {
         $show
-            ->add('name',TextType::class, ['label' => $this->getParamLabel('name')])
-            ->add('slug',TextType::class, ['label' => $this->getParamLabel('slug')])
+            ->add('name',TextType::class, ['label' => self::NAME_LABEL])
+            ->add('slug',TextType::class, ['label' => self::SLUG_LABEL])
             ->add('color',FieldDescriptionInterface::TYPE_CHOICE, [
                 'choices' => $this->getColors(),
-                'label' => $this->getParamLabel('color')
+                'label' => self::COLOR_LABEL
             ]);
-    }
-
-    private function getParamLabel($key): bool|string
-    {
-        $params = [
-            'name' => 'Название',
-            'slug' => 'Уникальный фрагмент URL-адреса',
-            'color' => 'Цвет',
-        ];
-        if (in_array($key, $params)) {
-            return $params[$key];
-        }
-        return false;
     }
 
     private function getColors(): array {
