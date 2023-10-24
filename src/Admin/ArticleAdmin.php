a0d3f718d46af0a8eeea0c7e826340a1efd64f22
Rumil <cannirumil@yandex.ru>
2023-10-24 11:44:09 +0300

    fixes

--- src/Admin/ArticleAdmin.php
+++ src/Admin/ArticleAdmin.php
@@ -2,13 +2,15 @@
 
 namespace App\Admin;
 
-use App\Entity\Category;
+use App\Entity\{ Category, Article };
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
@@ -18,13 +20,38 @@ final class ArticleAdmin extends AbstractAdmin
     private const SLUG_LABEL = 'Уникальный фрагмент URL-адреса';
     private const CONTENT_LABEL = 'Cодержание';
     private const CATEGORIES_LABEL = 'Категории';
+    private const FEATURED_TEXT_LABEL = 'Избранный текст';
+    private const CREATED_AT_LABEL = 'Дата создания';
+    private const UPDATED_AT_LABEL = 'Дата изменения';
 
     protected function configureFormFields(FormMapper $form): void
     {
+        if ($this->isCurrentRoute('create')) {
+            $form
+                ->add('createdAt', DateTimeType::class, [
+                    'data' => new \DateTime(),
+                    'label' => self::CREATED_AT_LABEL
+                ]);
+        } else {
+            $form
+                ->add('updatedAt', DateTimeType::class, [
+                    'data' => new \DateTime(),
+                    'label' => self::UPDATED_AT_LABEL
+                ]);
+        }
         $form
-            ->add('title', TextType::class, ['label' => self::NAME_LABEL])
-            ->add('slug', TextType::class, ['label' => self::SLUG_LABEL])
-            ->add('content', TextareaType::class, ['label' => self::CONTENT_LABEL])
+            ->add('title', TextType::class, [
+                'label' => self::NAME_LABEL
+            ])
+            ->add('slug', TextType::class, [
+                'label' => self::SLUG_LABEL
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => self::CONTENT_LABEL
+            ])
+            ->add('featuredText', TextType::class, [
+                'label' => self::FEATURED_TEXT_LABEL
+            ])
             ->add('categories', EntityType::class, [
                 'class' => Category::class,
                 'multiple' => true,
@@ -42,11 +69,58 @@ final class ArticleAdmin extends AbstractAdmin
 
     protected function configureListFields(ListMapper $list): void
     {
-        $list->addIdentifier('title');
+        $list
+            ->addIdentifier('title', TextType::class, [
+                'label' => self::NAME_LABEL
+            ])
+            ->add('featuredText', TextType::class, [
+                'label' => self::FEATURED_TEXT_LABEL
+            ])
+            ->add('createdAt', null, [
+                'label' => self::CREATED_AT_LABEL
+            ])
+            ->add('updatedAt', null, [
+                'label' => self::UPDATED_AT_LABEL
+            ])
+            ->add('categories', null, [
+                'associated_property' => 'name',
+                'delimiter' => ' | ',
+                'label' => self::CATEGORIES_LABEL
+            ])
+            ->add(ListMapper::NAME_ACTIONS, null, [
+                'actions' => [
+                    'show' => [],
+                    'edit' => [],
+                    'delete' => []
+                ]
+            ]);
     }
 
     protected function configureShowFields(ShowMapper $show): void
     {
-        $show->add('title');
+        $show
+            ->add('title', null, [
+                'label' => self::NAME_LABEL
+            ])
+            ->add('slug', null, [
+                'label' => self::SLUG_LABEL
+            ])
+            ->add('featuredText', null, [
+                'label' => self::FEATURED_TEXT_LABEL
+            ])
+            ->add('createdAt', null, [
+                'label' => self::CREATED_AT_LABEL
+            ])
+            ->add('updatedAt', null, [
+                'label' => self::UPDATED_AT_LABEL
+            ])
+            ->add('categories', null, [
+                'associated_property' => 'name',
+                'delimiter' => ' | ',
+                'label' => self::CATEGORIES_LABEL
+            ])
+            ->add('content', null, [
+                'label' => self::CONTENT_LABEL
+            ]);
     }
 }
--- src/Admin/CategoryAdmin.php
+++ src/Admin/CategoryAdmin.php
@@ -5,10 +5,9 @@ namespace App\Admin;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
-use Sonata\AdminBundle\FieldDescription\FieldDescriptionInterface;
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\AdminBundle\Form\Type\ChoiceFieldMaskType;
 use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 final class CategoryAdmin extends AbstractAdmin
@@ -20,48 +19,79 @@ final class CategoryAdmin extends AbstractAdmin
     protected function configureFormFields(FormMapper $form): void
     {
         $form
-            ->add('name', TextType::class, ['label' => self::NAME_LABEL])
-            ->add('slug', TextType::class, ['label' => self::SLUG_LABEL])
-            ->add('color', ChoiceFieldMaskType::class, [
-                'multiple' => false,
-                'choices' => array_flip($this->getColors()),
-                'label' => self::COLOR_LABEL
+            ->add('name', TextType::class, [
+                'label' => self::NAME_LABEL
+            ])
+            ->add('slug', TextType::class, [
+                'label' => self::SLUG_LABEL
+            ])
+            ->add('color', ColorType::class, [
+                'label' => self::COLOR_LABEL,
+                'attr' => [
+                    'style' => 'width: 100px;'
+                ]
             ]);
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagrid): void
     {
         $datagrid
-            ->add('name', null, [], ['label' => self::NAME_LABEL])
-            ->add('slug', null, [], ['label' => self::SLUG_LABEL])
-            ->add('color', null, [], ['label' => self::COLOR_LABEL]);
+            ->add('name', null, [], [
+                'label' => self::NAME_LABEL
+            ])
+            ->add('slug', null, [], [
+                'label' => self::SLUG_LABEL
+            ])
+            ->add('color', null, [], [
+                'label' => self::COLOR_LABEL
+            ]);
     }
 
     protected function configureListFields(ListMapper $list): void
     {
         $list
-            ->addIdentifier('name', TextType::class, ['label' => self::NAME_LABEL])
-            ->add('slug', TextType::class, ['label' => self::SLUG_LABEL])
-            ->add('color', TextType::class, ['label' => self::COLOR_LABEL]);
+            ->addIdentifier('name', TextType::class, [
+                'label' => self::NAME_LABEL
+            ])
+            ->add('slug', TextType::class, [
+                'label' => self::SLUG_LABEL
+            ])
+            ->add('color', ColorType::class, [
+                'label' => self::COLOR_LABEL,
+                'attr' => [
+                    'style' => 'width: 100px;'
+                ]
+            ])
+            ->add(ListMapper::NAME_ACTIONS, null, [
+                'actions' => [
+                    'show' => [
+                        'label' => 'Показать'
+                    ],
+                    'edit' => [
+                        'label' => 'Изменить'
+                    ],
+                    'delete' => [
+                        'label' => 'Удалить'
+                    ]
+                ],
+                'label' => 'Действия'
+            ]);
     }
 
     protected function configureShowFields(ShowMapper $show): void
     {
         $show
-            ->add('name',TextType::class, ['label' => self::NAME_LABEL])
-            ->add('slug',TextType::class, ['label' => self::SLUG_LABEL])
-            ->add('color',FieldDescriptionInterface::TYPE_CHOICE, [
-                'choices' => $this->getColors(),
-                'label' => self::COLOR_LABEL
+            ->add('name',TextType::class, [
+                'label' => self::NAME_LABEL
+            ])
+            ->add('slug',TextType::class, [
+                'label' => self::SLUG_LABEL
+            ])
+            ->add('color',ColorType::class, [
+                'label' => self::COLOR_LABEL,
+                'attr' => [
+                    'style' => 'width: 100px;'
+                ]
             ]);
-    }
-
-    private function getColors(): array {
-        return [
-            'red' => 'Красный',
-            'green' => 'Зеленый',
-            'blue' => 'Синий',
-            'black' => 'Черный'
-        ];
     }
 }
